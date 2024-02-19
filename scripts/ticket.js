815dbcc135815b2cf322e472953a79e7cd2e87a6
khalid09dev <mdkhalidhossen@hotmail.com>
2024-02-19 09:25:42 +0600

    added seat selecting funcitionality

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,6 +1,15 @@
 function seatSelected() {
     
-    const seat = document.getElementById('seat-left');
-    const seatCount = seat.innerHTML - 1;
-    // const newSeatNumber = newSeatNumber.ad
+    const availableSeat = document.getElementById('seat-left');
+    const newSeat = Math.floor(availableSeat.innerText - 1);
+    availableSeat.innerText = newSeat;
+    
+    const seatsLeft = document.getElementById('seat-left-now');
+    seatsLeft.classList.remove('bg-[#F7F8F8]');
+    
+    const seatLeft = document.getElementById('seat-left-now');
+    seatLeft.classList.add('bg-[#1DD100]', 'text-white');
+
+    const seatSelected = document.getElementById('seat-selected');
+    seatSelected.classList.add('text-[#1DD100]', 'font-medium');
 }
